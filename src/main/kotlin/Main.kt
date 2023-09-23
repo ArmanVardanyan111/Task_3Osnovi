cca0235a2e5ce312f98ce2d3d1290b7bd009899f
ArmanVardanyan111 <129722814+ArmanVardanyan111@users.noreply.github.com>
2023-09-23 12:09:46 +0300

    rewqgqwer

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,3 @@
-/*fun main(args: Array<String>) {
-    var text = "136"
-    var text_ = ""
-    var x = text.toInt()
-
-    while(x != 1)
-    {
-        var a = x/2
-        x -= a*2
-        text_ += x
-        x = a
-    }
-    text_ += x
-
-    print(text_)
-}*/
-
 fun main(args: Array<String>) {
     var text = "2"
     var text_ = ""
@@ -28,5 +11,6 @@ fun main(args: Array<String>) {
         x = a
     }
     text_ += x
+
     print(text_.reversed())
 }
